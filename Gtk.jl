5c352e3adff393c9b6149d490a15333fecf961b6
Hariib00 <99896525+Hariib00@users.noreply.github.com>
2022-02-17 12:31:47 -0500

    Add files via upload

---
+++ Gtk.jl
@@ -0,0 +1,97 @@
+using Gtk
+using Sound: sound
+using WAV: wavread
+using WAV: wavwrite
+
+S = 8192 # sampling rate (samples/second) for this low-fi project
+tone = Float32[] # initialize "song" as an empty vector
+
+function miditone(midi::Int; nsample::Int = 2000)
+    f = midi # compute frequency from midi number - FIXME!
+    x = cos.(2pi*(1:nsample)*f/S) # generate sinusoidal tone
+    sound(x, S) # play note so that user can hear it immediately
+    global tone = [tone; x] # append note to the (global) song vector
+    return nothing
+end
+
+# define the white and black keys and their midi numbers - FIXME!
+first_line = ["1" 50 500; "2" 50 500; "3" 50 500]
+second_line = ["4" 50 500; "5" 50 500; "6" 50 500]
+third_line = ["7" 50 500; "8" 50 500; "9" 50 500]
+fourth_line = ["*" 50 500; "0" 50 500; "#" 50 500]
+
+g = GtkGrid() # initialize a grid to hold buttons
+set_gtk_property!(g, :row_spacing, 5) # gaps between buttons
+
+set_gtk_property!(g, :column_spacing, 5)
+set_gtk_property!(g, :row_homogeneous, true) # stretch with window resize
+set_gtk_property!(g, :column_homogeneous, true)
+
+
+sharp = GtkCssProvider(data="#wb {color:black; background:white;}")
+endbut = GtkCssProvider(data="#wb {color:black; background:white;}")
+clearbut = GtkCssProvider(data="#wb {color:black; background:red;}")
+
+for i in 1:size(first_line,1) # add the white keys to the grid
+    key, midi = first_line[i,1:3]
+    b = GtkButton(key) # make a button for this key
+    push!(GAccessor.style_context(b), GtkStyleProvider(sharp), 600)
+    set_gtk_property!(b, :name, "wb") 
+    signal_connect((w) -> miditone(midi), b, "clicked") # callback
+    g[(1:2) .+ 2*(i-1), 1] = b 
+end
+
+for i in 1:size(second_line,1) 
+    key, midi = second_line[i,1:3]
+    b = GtkButton(key) # to make ] symbol, type \sharp then hit <tab>
+    push!(GAccessor.style_context(b), GtkStyleProvider(sharp), 600)
+    set_gtk_property!(b, :name, "wb") # set "style" of black key
+    signal_connect((w) -> miditone(midi), b, "clicked") # callback
+    g[(1:2) .+ 2*(i-1), 2] = b # put the button in row 1 of the grid
+end
+
+for i in 1:size(third_line,1) # add the black keys to the grid
+    key, midi= third_line[i,1:3]
+    b = GtkButton(key) # to make ] symbol, type \sharp then hit <tab>
+    push!(GAccessor.style_context(b), GtkStyleProvider(sharp), 600)
+    set_gtk_property!(b, :name, "wb") # set "style" of black key
+    signal_connect((w) -> miditone(midi), b, "clicked") # callback
+    g[(1:2) .+ 2*(i-1), 3] = b # put the button in row 1 of the grid
+end
+
+for i in 1:size(fourth_line,1) # add the black keys to the grid
+    key, midi= fourth_line[i,1:3]
+    b = GtkButton(key) # to make ] symbol, type \sharp then hit <tab>
+    push!(GAccessor.style_context(b), GtkStyleProvider(sharp), 600)
+    set_gtk_property!(b, :name, "wb") # set "style" of black key
+    signal_connect((w) -> miditone(midi), b, "clicked") # callback
+    g[(1:2) .+ 2*(i-1), 4] = b # put the button in row 1 of the grid
+end
+
+function end_button_clicked(w) # callback function for "end" button
+    println("The end button")
+    sound(tone, S) # play the entire song when user clicks "end"
+    wavwrite(tone, "proj2.wav"; Fs=S) # save song to file
+end
+
+function clear_button_clicked(w)
+    println("The clear button")
+    global tone = Float32[];
+end
+
+ebutton = GtkButton("dial") # make an "end" button
+g[1:4, 5] = ebutton # fill up entire row 3 of grid - why not?
+clearbutton = GtkButton("clear")
+g[4:6, 5] = clearbutton
+
+set_gtk_property!(clearbutton, :name, "wb")
+signal_connect(clear_button_clicked, clearbutton, "clicked")
+push!(GAccessor.style_context(clearbutton), GtkStyleProvider(clearbut), 600)
+signal_connect(end_button_clicked, ebutton, "clicked") # callback
+
+push!(GAccessor.style_context(ebutton), GtkStyleProvider(endbut), 400)
+set_gtk_property!(ebutton, :name, "wb")# FIXME! set style of the "end" button
+
+win = GtkWindow("gtk3", 400, 300) # 400×300 pixel window for all the buttons
+push!(win, g) # put button grid into the window
+showall(win); # display the window full of buttons
