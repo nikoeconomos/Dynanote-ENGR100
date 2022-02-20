c266b90bf8aecb81924c7a5bcfd3e2484cdbe1bf
Nate Sochocki <natesoch@umich.edu>
2022-02-20 18:33:20 -0500

    Started noise stuff

--- Proj2_noise.jl
+++ Proj2_noise.jl
@@ -3,9 +3,22 @@ using WAV: wavread
 using FFTW; fft
 using Plots
 
-file = "project2.wav"
-(X, S, _, _) = wavread(file)
-soundsc(X, S)
-@show N = length(X)
+S = 8192
+x = cos.(2pi*(1:4096)*696/S) .+ cos.(2pi*(1:4096)*1210/S) #signal for button 1?
+errors = zeros(Int, 10)
+snr  = zeros(10)
 
-SNR = 10*log10(sum(x.^2) / sum(noise.^2))
+for level=1:10 #10 different noise levels
+    noisesum = 0
+    for trial=1:100 # 100 trials for each noise level
+        noise  = 5 * level * randn(size(x))
+        y = x + noise
+        noise += sum(noise.^2)
+
+        if "output does != 1"
+            errors[level] += 1
+        end
+    end
+    snr[level] = 10*log10(sum(x.^2) / (noisesum/100))
+end
+plot(snr, errors, marker=:circle, xlabel = "SNR [dB]", ylabel = "% errors")
