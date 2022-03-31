160633cdb4d2b0b31137cbbe89d9370728542899
Nate Sochocki <natesoch@umich.edu>
2022-03-31 10:35:50 -0400

    Update Phase_vocoder.jl

--- Project 2/Phase_vocoder.jl
+++ Project 2/Phase_vocoder.jl
@@ -17,22 +17,47 @@ function sample_record(seconds, octaves, steps, pitch::String)
     if pitch == "Decrease"
        
         N = length(data)
-
-        #factor = 2^(steps/12) + octaves
+        
+        octaves = octaves - 1
+        @show octaves + (2^(steps/12))
+        N2 = round(Int, N * (octaves + (2^(steps/12))))
 
         mod(N,2) == 0 || throw("N must be multiple of 2")
         F = fft(data) # original spectrum
-        Fnew = [F[1:N÷2]; zeros(N * octaves); F[(N÷2+1):N]]
+        Fnew = [F[1:N÷2]; zeros(N2); F[(N÷2+1):N]]
         Snew = 2 * real(ifft(Fnew))[1:N]
         @show length(Snew)
         soundsc(Snew, S)
     
     elseif pitch == "Increase"
         #needs to increase by one for hopin/hopout ratio
-        octaves +=1
-        y = phase_vocoder(data, S; hopin=121, hopout=(octaves*121))
-        Y = y[1:octaves:end]
-        @show length(Y)
-        soundsc(Y,S)
+        # octaves +=1
+        # y = phase_vocoder(data, S; hopin=121, hopout=(octaves*121))
+        # Y = y[1:octaves:end]
+        # @show length(Y)
+        # soundsc(Y,S)
+
+        if steps > 0
+            octaves +=2
+            y = phase_vocoder(data, S; hopin=121, hopout=(octaves*121))
+            Y = y[1:octaves:end]
+            @show length(Y)
+            soundsc(Y,S)
+            
+            N2 = round(Int, N * (0 + (2^(steps/12))))
+
+            mod(N,2) == 0 || throw("N must be multiple of 2")
+            F = fft(Y) # original spectrum
+            Fnew = [F[1:N÷2]; zeros(N2); F[(N÷2+1):N]]
+            Snew = 2 * real(ifft(Fnew))[1:N]
+            @show length(Snew)
+            soundsc(Snew, S)
+        else
+            octaves +=1
+            y = phase_vocoder(data, S; hopin=121, hopout=(octaves*121))
+            Y = y[1:octaves:end]
+            @show length(Y)
+            soundsc(Y,S)
+        end
     end
 end
