b8af23a6c730ea351c747ccf4b6ac280e4ffe1c3
nikoeconomos <niko.economos64@gmail.com>
2022-03-31 14:57:22 -0400

    sustain adds

--- Sustain.jl
+++ Sustain.jl
@@ -1,10 +1,11 @@
 #Niko Economos, ENGR 100
-using Sound: phase_vocoder, soundsc, hann, record
+using Sound: soundsc, record
 using DSP: spectrogram
 using MIRTjim: jim, prompt
 using InteractiveUtils: versioninfo
 using FFTW: fft, ifft
 using WAV: wavread
+using Plots; default(label="")
 
 function sustain()
     S = 44100
@@ -15,14 +16,26 @@ function sustain()
     # data = cos.(2*pi*440*t)
     # @show length(data)
 
-    file = "piano_note.wav" # adjust as needed
+    file = "piano_note.wav"
     (data, S, _, _) = wavread(file)
-    D2 = 2/N2 * fft(data, 1) # 1D fft of each column of y2
-    heatmap(abs.(D2), xlabel="time segment", ylabel="l=k+1") # spectrogram
+    data = data[:, 1]
+    data = vec(data)
+    N = length(data)
+    D2 = 2/N * fft(data, 1) # 1D fft of each column of y2
 
-    soundsc(data, S)
+    p1 = plot(abs.(D2), xlabel="frequency index l=k+1", ylabel="|Y[l]|")
+    xlims!(1, 1+N÷2);
+    p2 = heatmap(abs.(D2), xlabel="time segment", ylabel="l=k+1") # 
+    p3 = plot(data, ylabel="amplitude")
 
-    p = plot(vec(data), ylabel="f [Hz]")
+    #plot(p1, p2, p3)
+    plot(p2)
 
-    
+
+
+    #soundsc(data, S)
+
+
 end
+
+sustain()
