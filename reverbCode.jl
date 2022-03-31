487ebbe7bf23a06e8470bf41b96cc2bf7a031b2a
sravyaganti <99568351+sravyaganti@users.noreply.github.com>
2022-03-31 12:38:03 -0400

    Update reverbCode.jl

--- reverbCode.jl
+++ reverbCode.jl
@@ -1,16 +1,35 @@
 using Sound
+using Plots
+using WAV: wavread
 
-# S = 44100
-# N = Int(0.5 * S) # 0.5 sec
-# t = (0:N-1)/S # time samples: t = n/S
-# y = 0.5 * cos.(2π * 400 * t);
-S=44100
-t = (1:S/2)/S
-data = cos.(2*pi*400*t)
-soundsc(data,S);
-D= 10;
+
+ S=44100
+ t = (1:S/2)/S
+ data = cos.(2*pi*400*t)
+ soundsc(data,S);
+
+
+D= 1000;
 L= length(data);
-s= data[1:L-2*D] + data[1+D:L-D] + data[1+2*D:L] + data[1+2*D:L]
-s2= reshape(s, 22030, :)
+show(L)
+print("\n")
+show(length(data[1:L-6*D]))
+print("\n")
+show(length(data[1+D:L-5*D]))
+print("\n")
+show(length(data[1+2*D:L-4*D]))
+print("\n")
+show(length(data[1+3*D:L-3*D]))
+print("\n")
+show(length(data[1+4*D:L-2*D]))
+print("\n")
+show(length(data[1+5*D:L-D]))
+print("\n")
+show(length(data[1+6*D:L]))
+print("\n")
+s= data[1:L-6*D] + data[1+D:L-5*D] + data[1+2*D:L-4*D]+data[1+3*D:L-3*D]+data[1+4*D:L-2*D]+data[1+5*D:L-D]+data[1+6*D:L];
+#X=length(s)
+s2= data+s;
 soundsc(s2,S);
+show(length(s))
 
