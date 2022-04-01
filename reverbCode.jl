839ea41133b79a4b8bb8729268dfb990853ef6f2
sravyaganti <99568351+sravyaganti@users.noreply.github.com>
2022-04-01 10:54:32 -0400

    Update reverbCode.jl

--- reverbCode.jl
+++ reverbCode.jl
@@ -6,10 +6,10 @@ using WAV: wavread
  S=44100
  t = (1:S/2)/S
  data = cos.(2*pi*400*t)
- soundsc(data,S);
+#soundsc(data,S);
 
 
-D= 1000;
+D= 2000;
 L= length(data);
 show(L)
 print("\n")
@@ -27,9 +27,9 @@ show(length(data[1+5*D:L-D]))
 print("\n")
 show(length(data[1+6*D:L]))
 print("\n")
-s= data[1:L-6*D] + data[1+D:L-5*D] + data[1+2*D:L-4*D]+data[1+3*D:L-3*D]+data[1+4*D:L-2*D]+data[1+5*D:L-D]+data[1+6*D:L];
-#X=length(s)
-s2= data+s;
-soundsc(s2,S);
-show(length(s))
+s= data[1:L-6*D] + data[1+D:L-5*D] + data[1+2*D:L-4*D]+ data[1+3*D:L-3*D]+ data[1+4*D:L-2*D]+ data[1+5*D:L-D]+data[1+6*D:L];
+sLength= length(s);
+l=L=sLength;
+soundsc(s, 44100);
+
 
