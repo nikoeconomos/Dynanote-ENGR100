353dfba769f40d6cf2f11e30a9de51dc6b79a64b
iansconcannon <100086386+iansconcannon@users.noreply.github.com>
2022-04-06 09:53:43 -0400

    Update soundenv.jl

--- soundenv.jl
+++ soundenv.jl
@@ -1,24 +1,29 @@
 # sound envelope modifications
+using MIRT: interp1
+
+function staccato()
+
+  S = 44100
+  N = Int(1 * S)
+  t = (0:N-1)/S
+  c = 1 ./ (1:2:15) # amplitudes
+  f = (1:2:15) * 494 # frequencies
+  x = +([c[k] * sin.(2π * f[k] * t) for k in 1:length(c)]...) # !!
+  env = (1 .- exp.(-80*t)) .* exp.(-3*t) # fast attack; slow decay
+  y = env .* x
+
+end
+
 
 function asdr()
-
-S = 44100
-N = Int(1 * S)
-t = (0:N-1)/S
-c = 1 ./ (1:2:15) # amplitudes
-f = (1:2:15) * 494 # frequencies
-x = +([c[k] * sin.(2π * f[k] * t) for k in 1:length(c)]...) # !!
-env = (1 .- exp.(-80*t)) .* exp.(-3*t) # fast attack; slow decay
-y = env .* x
-
-using MIRT: interp1 # one of many Julia interpolators
-S = 44100
-N = Int(1.5 * S)
-t = (0:N-1)/S
-c = 1 ./ (1:2:15) # amplitudes
-f = (1:2:15) * 494 # frequencies
-x = sin.(2π * t * f') * c
-env = interp1([0, 0.1, 0.3, 1.1, 1.5], [0, 1, 0.4, 0.4, 0], t) # !! 
-y = env .* x
+  
+  S = 44100
+  N = Int(1.5 * S)
+  t = (0:N-1)/S
+  c = 1 ./ (1:2:15) # amplitudes
+  f = (1:2:15) * 494 # frequencies
+  x = sin.(2π * t * f') * c
+  env = interp1([0, 0.1, 0.3, 1.1, 1.5], [0, 1, 0.4, 0.4, 0], t) # !! 
+  y = env .* x
 
 end
