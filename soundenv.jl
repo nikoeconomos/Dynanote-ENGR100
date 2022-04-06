95f0da9db188b6d821917c7c25f2e0a9d44b7246
iansconcannon <100086386+iansconcannon@users.noreply.github.com>
2022-04-06 10:29:34 -0400

    Update soundenv.jl

--- soundenv.jl
+++ soundenv.jl
@@ -1,29 +1,41 @@
-# sound envelope modifications
 using MIRT: interp1
+using Sound: soundsc
+using Plots
+using WAV: wavread
 
-function staccato()
+time = 100
+freq = 460
+type = "asdr"
+y
+S = 44100
 
-  S = 44100
-  N = Int(1 * S)
-  t = (0:N-1)/S
-  c = 1 ./ (1:2:15) # amplitudes
-  f = (1:2:15) * 494 # frequencies
-  x = +([c[k] * sin.(2π * f[k] * t) for k in 1:length(c)]...) # !!
-  env = (1 .- exp.(-80*t)) .* exp.(-3*t) # fast attack; slow decay
-  y = env .* x
+function soundenv(time, freq, type)
+
+
+    if type == "staccato"
+        S = 44100
+        N = Int(time * S)
+        t = (0:N-1)/S
+        c = 1 ./ (1:2:1000) # amplitudes    
+        f = (1:2:1000) * freq # frequencies
+        x = +([c[k] * sin.(2π * f[k] * t) for k in 1:length(c)]...) # !!
+        env = (time .- exp.(-80*t)) .* exp.(-3*t) # fast attack; slow decay
+        y = env .* x
+        
+    end
+
+    if type == "asdr"
+        S = 44100
+        N = Int(time * S)
+        t = (0:N-1)/S
+        c = 1 ./ (1:2:1000) # amplitudes
+        f = (1:2:1000) * freq # frequencies
+        x = sin.(2π * t * f') * c
+        env = interp1([0, time * 0.1, time * 0.2, time * 0.85, time], [0, 1, 0.3, 0.3, 0], t) # !! 
+        y = env .* x
+        
+    end
 
 end
 
-
-function asdr()
-  
-  S = 44100
-  N = Int(1.5 * S)
-  t = (0:N-1)/S
-  c = 1 ./ (1:2:15) # amplitudes
-  f = (1:2:15) * 494 # frequencies
-  x = sin.(2π * t * f') * c
-  env = interp1([0, 0.1, 0.3, 1.1, 1.5], [0, 1, 0.4, 0.4, 0], t) # !! 
-  y = env .* x
-
-end
+soundsc(y, S)
