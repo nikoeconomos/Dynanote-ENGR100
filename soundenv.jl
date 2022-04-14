d29741fc43e9cf311f0d10d86fa0349fbb680e63
iansconcannon <iansc@umich.edu>
2022-04-14 12:00:50 -0400

    Update soundenv.jl

--- soundenv.jl
+++ soundenv.jl
@@ -10,22 +10,26 @@ file = "/Users/ian/Documents/GitHub/engr100-trombones/piano_note.wav"
 data = x[:,1]
 soundsc(data, S)
 
+N = length(data)
+time = (0:N-1)/S
+
 function fast_attack_decay(stuff)
-    N = length(data)
+    N = length(stuff)
     time = (0:N-1)/S
     env = (time .- exp.(-80*time)) .* exp.(-3*time) # fast attack; slow decay
-    y = env .* stuff
+    y = 8 * env .* stuff
     return y
 end
 
 function asdr(stuff)
-    N = length(data)
+    N = length(stuff)
     time = (0:N-1)/S
-    a = time * 0.1
-    b = time * 0.2
-    c = time * 0.85
-    d = time * 1
-    env = interp1([0, a, b, c, d], [0, 1, 0.3, 0.3, 0], time) # !! 
+    t = (N-1)/S
+    a = t * 0.12
+    b = t * 0.24
+    c = t * 0.84
+    d = t * 1
+    env = interp1([0, a, b, c, d], [0, 1, 0.4, 0.4, 0], time)
     y = env .* stuff
     return y
 end
@@ -33,3 +37,4 @@ end
 #lol = fast_attack_decay(data)
 lol = asdr(data)
 soundsc(lol, S)
+
